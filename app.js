3d0ce1a24d4c41cc076fafc382f2fcedd64be7da
Karthik Viswanathan <karthik.ksv@gmail.com>
2015-11-30 19:01:11 +0530

    Complete soundcloud.

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var express = require('express');
+var soundcloud = require('./lib/soundcloud');
+
+var STATUS_OK = 200;
+var STATUS_USER_ERROR = 422;
 
 var app = express();
 // serve all files out of public folder
 app.use(express.static('public'));
 
-// TODO: soundcloud finder code
+app.get('/search', function(request, response) {
+  var title = request.query.title;
+  var artist = request.query.artist;
+
+  if (!title || !artist) {
+    // missing required parameters
+    response.set('Content-type', 'text/plain');
+    response.status(STATUS_USER_ERROR);
+    response.send('You must specify both a title and artist.');
+  } else {
+    soundcloud.search(title, artist, function(error, tracks) {
+      if (error) {
+        throw error;
+      }
+
+      response.set('Content-type', 'application/json');
+      response.status(STATUS_OK);
+      response.send(tracks);
+    });
+  }
+});
 
 var port = 3000;
 console.log('Listening at 127.0.0.1:' + port);
--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,35 @@ var STATUS_OK = 200;
 SC.initialize({ client_id: SC_CLIENT_ID });
 var $player = $('#player');
 
-// TODO: soundcloud finder code
+$("#search").submit(function() {
+  event.preventDefault();
+
+  var title = $('#search input[name="title"]').val();
+  var artist = $('#search input[name="artist"]').val();
+
+  $player.text('Loading...');
+
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', function() {
+    if (request.status === STATUS_OK) {
+      var tracks = JSON.parse(request.responseText);
+
+      if (tracks.length === 0) {
+        $player.text('No tracks were found.');
+      } else {
+        displayPlayer(tracks[0]);
+      }
+    } else {
+      $player.text(request.responseText);
+    }
+  });
+
+  // make request to our search endpoint
+  var searchParams = 'artist=' + encodeURIComponent(artist) +
+    '&title=' + encodeURIComponent(title);
+  request.open('GET', '/search?' + searchParams);
+  request.send();
+});
 
 /* Display SoundCloud player for the given track. */
 function displayPlayer(track) {
