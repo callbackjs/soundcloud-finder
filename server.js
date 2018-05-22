8cda712b60425205825466ceec072f47034b2beb
Vivek Nair <vnair@avinetworks.com>
2018-05-22 15:54:49 -0700

    Rename from app to server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const app = express()
+const server = express()
 // serve all files out of public folder
-app.use(express.static('public'))
+server.use(express.static('public'))
 
 // TODO: soundcloud finder code
 
 const port = 3000
 console.log('Listening at 127.0.0.1:' + port)
-app.listen(port)
+server.listen(port)
